f7d442c1d9ab6a466937eed0248f6943bea00122
Steven Dzilvelis <stevedz@twitter.com>
2015-11-10 22:03:59 -0700

    Updated to handle quotes in Tweets, added header row, fixed progress bar

--- Create-CombinedCsv.ps1
+++ Create-CombinedCsv.ps1
@@ -17,7 +17,6 @@ $comma = ","
 
 $bytesRead = 0
 
-
 try
 {
 	# Add references for HttpUtility
@@ -27,14 +26,17 @@ try
 	$writer = [System.IO.File]::CreateText($CSVFilePath)
 	$Encode = [System.Web.HttpUtility]::UrlEncode($URL) 
 
-	 # $index = 0
+	$index = 0
+
+	$writer.WriteLine( "id,body,language,postTime,shape,p1,p2,p3,p4")
 
 	for(;;) {
 	    	$line = $reader.ReadLine()
    	 	if ($line -eq $null) { break }
  	   	
 		# process the line
- 	   	$bytesRead += $line.Length
+ 	   	$bytesRead += $line.Length	
+
  	   	$obj = ConvertFrom-Json $line
     
  	   	# create a blank polygon array to eliminate nulls
@@ -46,25 +48,35 @@ try
 			$geo = $location.geo.coordinates
     		}
     	
-    		Write-Progress -Activity "Export to CSV" -status $bytesRead -percentComplete ($bytesRead / $ActivityFileSize) -Id 1
+    		Write-Progress -Activity "Export to CSV" -status ('Row:' + $index) -percentComplete (($bytesRead / $ActivityFileSize) * 100) -Id 1
+
+		$geoText = ""
+		$body = $obj.body.Replace($quote, $quote + $quote)
+
+                if ($location.geo.type -eq "point")
+		{
+			$geoText = $geo[0][0] 
+		}
+		if ($location.geo.type -eq "polygon")
+		{
+			$geoText = $geo[0][0] + $comma +
+			$geo[0][1] + $comma +
+			$geo[0][2] + $comma +
+			$geo[0][3] 
+		}
     
     		$writer.WriteLine(
 			$quote + $obj.id + $quote  + $comma +
-			$quote + $obj.body + $quote + $comma + 
+			$quote + $body + $quote + $comma + 
 			$quote + $obj.twitter_lang + $quote + $comma + 
 			$obj.postedTime + $comma + 
 			$quote + $location.geo.type + $quote + $comma +
-			$geo[0][0] + $comma +
-			$geo[0][1] + $comma +
-			$geo[0][2] + $comma +
-			$geo[0][3] + $comma +
-			$geo[0][4] + $comma +
-			$geo[0][5] + $comma +
-			$geo[0][6] + $comma +
-			$geo[0][7] 
+			$geoText
 		)
 
-		# $index +=1
+		$index +=1
+
+		# used for testing
 		# if ( $index -gt 100 ) { break }
 	}
 
@@ -74,8 +86,10 @@ catch [System.Net.WebException],[System.Exception]
 	$ex = $_
 	write-host -foregroundcolor Red "Error exporting to CSV: " -nonewline
 	write-host -foregroundcolor Yellow $_
-	write-host -foregroundcolor Red "Line number:" -nonewline
+	write-host -foregroundcolor Red "Script line number:" -nonewline
 	write-host -foregroundcolor Yellow $ex.InvocationInfo.ScriptLineNumber
+	write-host -foregroundcolor Yellow "Processsing row:" -nonewline
+	write-host -foregroundcolor White $index -nonewline
 }
 try
 {
